b6318faa9b08984275505311f43f5fa7e11a8f45
Alex <alexander.g@btigroup.io>
2019-06-22 00:28:30 +0300

    Move files to new folders structure

---
+++ Coursera/JavaScript_part2/week3/worker.js
@@ -0,0 +1,13 @@
+var Collection = require('./index');
+
+// var numbers = new Collection();
+// numbers.append(10);
+// numbers.append(20);
+    
+// console.log(numbers.at(2)); // 20
+// console.log(numbers.values()); // [10, 20] 
+// console.log(numbers.count()); // 2
+// console.log(numbers.removeAt(1)); // 10
+
+var d = Collection.from([1,2,3]);
+console.info(d instanceof Collection);
