2b00519f8a8d5ee3f89ddf231e1f963278f45c81
Alex <alexander.g@btigroup.io>
2019-06-22 00:21:23 +0300

    Fixed task code

--- JavaScript_part2/week2/index.js
+++ JavaScript_part2/week2/index.js
@@ -9,39 +9,47 @@ function Collection() {
 }
 
 // Методы коллекции
-Collection.prototype.values = function () {
-    return this.innerCollection;
-};
+Collection.prototype = {
+    values: function () {
+        return this.innerCollection;
+    },
 
-Collection.prototype.append = function (value) {
-    this.innerCollection.push(value);
-}
+    append: function (value) {
+        if (value.constructor.name === 'Collection') {
+            this.innerCollection = this.innerCollection.concat(value.values());
+        } else {
+            this.innerCollection.push(value);
+        }
+    },
 
-Collection.prototype.at = function (index) {
-    return this.innerCollection[index - 1] || null;
-}
+    at: function (index) {
+        return this.innerCollection[index - 1] || null;
+    },
 
-Collection.prototype.count = function () {
-    return this.innerCollection.length;
-}
+    count: function () {
+        return this.innerCollection.length;
+    },
 
-Collection.prototype.removeAt = function (index) {
-    var idx = index - 1;
-    if (this.innerCollection[idx]) {
-        this.innerCollection.splice(idx, 1);
-        return true;
+    removeAt: function (index) {
+        var idx = index - 1;
+        if (this.innerCollection[idx]) {
+            this.innerCollection.splice(idx, 1);
+            return true;
+        }
+        return false;
     }
-    return false;
-}
+};
+Collection.prototype.constructor = Collection;
 
 
 /**
  * Создание коллекции из массива значений
  */
 Collection.from = function () {
+    var col = new Collection();
+    var initialArray = [];
     if (arguments && arguments[0] && Array.isArray(arguments[0]))
-        this.innerCollection = [].slice.call(arguments[0]);
-    else
-        this.innerCollection = [];
-    return this;
+        initialArray = [].slice.call(arguments[0]);
+    initialArray.forEach(item => col.append(item));
+    return col;
 };
