299acdb0a2ed0771ca4ec25c1e228a959be83b06
Alex <alexander.g@btigroup.io>
2019-06-22 22:31:30 +0300

    Solution with  Promise

--- Coursera/JavaScript_part2/week3/index.js
+++ Coursera/JavaScript_part2/week3/index.js
@@ -2,33 +2,63 @@
  * @param {Function[]} operations
  * @param {Function} callback
  */
-// solution with callbacks
+// solution with promises
 module.exports = function (operations, callback) {
 
+    let promises = [];
     let results = new Array(operations.length);
-    let globalError = null;
-    let executions = 0;
 
-    function nextClosure(operationIndex) {
-        let idx = operationIndex;
-        return function internalNext(err, res) {
-            executions++;
-            if (!err)
-                results[idx] = res;
-            else
-                if (!globalError)
-                    globalError = err;
+    operations.forEach((o, i) => {
+        let promise = new Promise(function (resolve, reject) {
+            let idx = i;
+            o(function (err, res) {
+                if (err)
+                    reject(err);
+                else {
+                    resolve(res);
+                    results[idx] = res;
+                }
+            });
+        });
+        promises.push(promise);
+    });
 
-            if (executions === operations.length) {
-                if (globalError)
-                    callback(globalError);
-                else
-                    callback(null, results);
-            }
-        }
-    }
-    if (operations.length > 0)
-        operations.forEach((o, i) => o(nextClosure(i)));
-    else
-        callback(null, []);
+    Promise.all(promises)
+        .then(function (res) {
+            callback(null, res);
+        })
+        .catch(function (err) {
+            callback(err);
+        });
 };
+
+// solution with callbacks
+// module.exports = function (operations, callback) {
+
+//     let results = new Array(operations.length);
+//     let globalError = null;
+//     let executions = 0;
+
+//     function nextClosure(operationIndex) {
+//         let idx = operationIndex;
+//         return function internalNext(err, res) {
+//             executions++;
+//             if (!err)
+//                 results[idx] = res;
+//             else
+//                 if (!globalError)
+//                     globalError = err;
+
+//             if (executions === operations.length) {
+//                 if (globalError)
+//                     callback(globalError);
+//                 else
+//                     callback(null, results);
+//             }
+//         }
+//     }
+//     if (operations.length > 0)
+//         operations.forEach((o, i) => o(nextClosure(i)));
+//     else
+//         callback(null, []);
+// };
