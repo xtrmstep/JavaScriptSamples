2f676ae1f690d25e862781807ac88c928c4d27c5
Alex <alexander.g@btigroup.io>
2019-06-28 01:48:40 +0300

    Added text validation

--- Coursera/JavaScript_part2/week5/script.js
+++ Coursera/JavaScript_part2/week5/script.js
@@ -4,47 +4,50 @@
 var validateForm = (function () {
 
     function getValidator(validatorType, dataSet) {
-        // return validator
-        return function () {
-            return true;
-        };
-    }
-
-    function handlerInputBlur() {
-
-    }
-
-    function handlerFormSubmit(evt, styles) {
-        if (evt) {
-            evt.preventDefault();
-            var formElem = document.querySelector("#" + styles.formId);
-            var formInputs = formElem.querySelectorAll("input");
-            var hasError = false;
-
-            formInputs.forEach(input => {
-                if (!hasError) {
-                    var isValid = getValidator(input.dataset.validator, input.dataset);
-                    if (isValid(input.value)) {                        
-                        hasError = true;
-                        if (!input.classList.contains(styles.inputErrorClass))
-                            input.classList.add(styles.inputErrorClass);
-                    } else {
-                        input.classList.remove(styles.inputErrorClass);
-                    }
-                }
-            });
-
-            if (hasError) {
-                formElem.classList.remove(styles.formValidClass);
-                formElem.classList.add(styles.formInvalidClass);
-            } else {
-                formElem.classList.remove(styles.formInvalidClass);
-                formElem.classList.add(styles.formValidClass);
-            }
+        switch (validatorType) {
+            case 'letters':
+                return function (text) {
+                    var regex1 = /[A-Za-zА-Яа-я]+/;
+                    var match = regex1.exec(text);
+                    return match == text; // if same, then it's only required text
+                };
+            default:
+                return function () {
+                    return true;
+                };
         }
+    }
 
+    function validateInput(input, styles) {
+        var isValid = getValidator(input.dataset.validator, input.dataset);
+        if (isValid(input.value)) {
+            input.classList.remove(styles.inputErrorClass);
+            return true;
+        } else {
+            if (!input.classList.contains(styles.inputErrorClass))
+                input.classList.add(styles.inputErrorClass);
+            return false;
+        }
+    }
 
-        // run validation here
+    function handlerFormSubmit(styles) {
+        var formElem = document.querySelector("#" + styles.formId);
+        var formInputs = formElem.querySelectorAll("input");
+        var hasError = false;
+
+        formInputs.forEach(input => {
+            if (!hasError) {
+                hasError = !validateInput(input, styles);
+            }
+        });
+
+        if (hasError) {
+            formElem.classList.remove(styles.formValidClass);
+            formElem.classList.add(styles.formInvalidClass);
+        } else {
+            formElem.classList.remove(styles.formInvalidClass);
+            formElem.classList.add(styles.formValidClass);
+        }
     }
 
     return function (args) {
@@ -59,17 +62,33 @@ var validateForm = (function () {
 
         // submit --
         var onSubmit = function (evt) {
+            evt.preventDefault();
+
             var styles = {
                 formId: args.formId,
                 formValidClass: args.formValidClass,
                 formInvalidClass: args.formInvalidClass,
                 inputErrorClass: args.inputErrorClass
             };
-            handlerFormSubmit(evt, styles);
+            handlerFormSubmit(styles);
         };
 
-        formElem.addEventListener('submit', onSubmit);
+        var onBlur = function (evt) {
+            if (evt.target.tagName === 'INPUT') {
+                validateInput(evt.target);
+            }
+        };
+
+        var onFocus = function (evt) {
+            if (evt.target.tagName === 'INPUT') {
+                input.classList.remove(args.inputErrorClass);
+            }
+        };
+
         btnSave.addEventListener('click', onSubmit);
+        formElem.addEventListener('submit', onSubmit);
+        formElem.addEventListener('blur', onBlur);
+        formElem.addEventListener('focus', onFocus);
 
     };
 })();
