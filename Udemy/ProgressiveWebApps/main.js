c9f17761e3a5ffc68d51225958c4b43e7c12418b
Alexander Goida <xtrmstep@users.noreply.github.com>
2019-08-08 08:31:59 +0300

    Added few events to SW

--- Udemy/ProgressiveWebApps/main.js
+++ Udemy/ProgressiveWebApps/main.js
@@ -1,14 +1,25 @@
 (function () {
 
-    if (navigator.serviceWorker){
+    if (navigator.serviceWorker) {
         navigator.serviceWorker.register("/sw.js")
-        .then(function(registered){
-            console.log("registered");
-        })
-        .catch(function(){
-            console.log("error");
-        });
-    }else{
+            .then(function (registered) {
+                console.log("registered");
+            })
+            .catch(function () {
+                console.log("error");
+            });
+    } else {
         console.log("sw is disabled");
     }
+
+    setTimeout(() => {
+        fetch("http://www.omdbapi.com/?i=tt3896198&apikey=9a608d49")
+            .then(response => response.json())
+            .then(function (data) {
+                console.log(data);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    }, 11000);
 })();
--- Udemy/ProgressiveWebApps/sw.js
+++ Udemy/ProgressiveWebApps/sw.js
@@ -1,13 +1,26 @@
-/*
+self.addEventListener('install', (e) => {
+    let awaiter = new Promise((resolve) => {
+        setTimeout(resolve, 5000); // some long work
+    });
+    e.waitUntil(awaiter);
 
+    console.log("sw installed");
+});
 
-    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=9a608d49")
-        .then(response => response.json())
-        .then(function(data){
-            console.log(data);
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
-*/
+self.addEventListener('activate', (e) => {
+    let awaiter = new Promise((resolve) => {
+        setTimeout(resolve, 5000); // some long work
+    });
+    e.waitUntil(awaiter);
+
+    console.log("sw activated - new");
+});
+
+self.addEventListener('fetch', (e) => {
+    // inspect all fetch requests
+    console.log(e.request);
+    let customResponse = new Response("{text:'No way!'}");
+    e.respondWith(customResponse);
+});
+
 console.log("sw registered");
